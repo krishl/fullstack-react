04ca784e8d51d16fcf4522c9490a2543b6c95d80
github username <krishel.lasam@gmail.com>
2017-07-31 20:14:42 -0400

    create and render ProductList to page with babel [p88]

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,1 +1,14 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+); // The first argument is what we’d like to render. The second argument is where to render it
