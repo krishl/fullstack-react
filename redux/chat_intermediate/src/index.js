f37e911f8827a5946b094c830d375d6a2f00577f
github username <krishel.lasam@gmail.com>
2017-08-03 23:23:04 -0400

    import app into index.js [chat_intermediate]

--- redux/chat_intermediate/src/index.js
+++ redux/chat_intermediate/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 // Readers: To prepare this project for building along in `./App.js`:
 // [1] Comment out this line:
-import App from "./complete/App-17";
+// import App from "./complete/App-17";
 // [2] Un-comment this line:
-// import App from "./App";
+import App from "./App";
 
 import "./index.css";
 import "./semantic-dist/semantic.min.css";
