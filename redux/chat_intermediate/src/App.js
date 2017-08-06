544e8fbd197afdccaae5506996320536947343f9
github username <krishel.lasam@gmail.com>
2017-08-06 05:43:06 -0400

    broke up reducer function to delegate responsibility [chat_intermediate]

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -3,38 +3,50 @@ import { createStore } from 'redux';
 import uuid from 'uuid';
 
 function reducer(state, action) {
+  return {
+    activeThreadId: activeThreadIdReducer(state.activeThreadId, action),
+    threads: threadsReducer(state.threads, action),
+  };
+}
+
+function activeThreadIdReducer(state, action) {
+  if (action.type === 'OPEN_THREAD') {
+    return action.id;
+  } else {
+    return state;
+  }
+}
+
+function threadsReducer(state, action) {
   if (action.type === 'ADD_MESSAGE') {
     const newMessage = {
       text: action.text,
       timestamp: Date.now(),
       id: uuid.v4(),
     };
-    const threadIndex = state.threads.findIndex(
+    const threadIndex = state.findIndex(
       (t) => t.id === action.threadId
     )
-    const oldThread = state.threads[threadIndex];
+    const oldThread = state[threadIndex];
     const newThread = {
       ...oldThread,
       messages: oldThread.messages.concat(newMessage),
     };
 
-    return {
-      ...state,
-      threads: [
-        ...state.threads.slice(0, threadIndex),
-        newThread,
-        ...state.threads.slice(
-          threadIndex + 1, state.threads.length
-        ),
-      ],
-    };
+    return [
+      ...state.slice(0, threadIndex),
+      newThread,
+      ...state.slice(
+      threadIndex + 1, state.length
+      ),
+    ]
   } else if (action.type === 'DELETE_MESSAGE') {
-    const threadIndex = state.threads.findIndex(
+    const threadIndex = state.findIndex(
       (t) => t.messages.find((m) => (
         m.id === action.id
       ))
     );
-    const oldThread = state.threads[threadIndex];
+    const oldThread = state[threadIndex];
 
     const newThread = {
       ...oldThread,
@@ -43,21 +55,13 @@ function reducer(state, action) {
       )),
     };
 
-    return {
-       ...state,
-      threads: [
-        ...state.threads.slice(0, threadIndex),
-        newThread,
-        ...state.threads.slice(
-          threadIndex + 1, state.threads.length
-        ),
-      ],
-    };
-  } else if (action.type === 'OPEN_THREAD') {
-    return {
-      ...state,
-      activeThreadId: action.id,
-    };
+    return [
+      ...state.slice(0, threadIndex),
+      newThread,
+      ...state.slice(
+        threadIndex + 1, state.length
+      ),
+    ]
   } else {
     return state;
   }
