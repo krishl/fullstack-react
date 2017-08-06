609a3343e6f325cda1bc3f1ac79ed7fbf3b710b6
github username <krishel.lasam@gmail.com>
2017-08-06 01:35:32 -0400

    support threads in initialState [chat_intermediate]

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -23,7 +23,26 @@ function reducer(state, action) {
   }
 }
 
-const initialState = { messages: [] };
+const initialState = { 
+  activeThreadId: '1-fca2', // New state property
+  threads: [ // Two threads in state
+    {
+      id: '1-fca2', // hardcoded pseudo-UUID
+      title: 'Buzz Aldrin',
+      messages: [
+        { // This thread starts with a single message already
+          text: 'Twelve minutes to ignition.',
+          timestamp: Date.now(),
+          id: uuid.v4(),
+        },
+      ],
+    },
+    {
+      id: '2-be91',
+      title: 'Michael Collins',
+      messages: [],
+    },
+  ], };
 
 const store = createStore(reducer, initialState);
 
