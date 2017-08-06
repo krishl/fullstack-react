424890bd4ef34d2dba3e8bceaa956ab5c337cf00
github username <krishel.lasam@gmail.com>
2017-08-06 05:49:31 -0400

    create messagesReducer() [chat_intermediate]

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -62,6 +62,19 @@ function threadsReducer(state, action) {
   }
 }
 
+function messagesReducer(state, action) {
+  if (action.type === 'ADD_MESSAGE') {
+    const newMessage = {
+      text: action.text,
+      timestamp: Date.now(),
+      id: uuid.v4(),
+    };
+    return state.concat(newMessage);
+  } else {
+    return state;
+  }
+}
+
 const initialState = { 
   activeThreadId: '1-fca2', // New state property
   threads: [ // Two threads in state
