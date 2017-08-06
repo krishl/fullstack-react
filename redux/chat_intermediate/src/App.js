16cbbce8fd1b32b9e5e174e2a9c645eb18a9ece4
github username <krishel.lasam@gmail.com>
2017-08-06 04:49:52 -0400

    update DELETE_MESSAGE reducer [chat_intermediate]

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -29,10 +29,29 @@ function reducer(state, action) {
       ],
     };
   } else if (action.type === 'DELETE_MESSAGE') {
-    return {
-      messages: state.messages.filter((m) => (
-        m.id !== action.id
+    const threadIndex = state.threads.findIndex(
+      (t) => t.messages.find((m) => (
+        m.id === action.id
       ))
+    );
+    const oldThread = state.threads[threadIndex];
+
+    const newThread = {
+      ...oldThread,
+      messages: oldThread.messages.filter((m) => (
+        m.id !== action.id
+      )),
+    };
+
+    return {
+       ...state,
+      threads: [
+        ...state.threads.slice(0, threadIndex),
+        newThread,
+        ...state.threads.slice(
+          threadIndex + 1, state.threads.length
+        ),
+      ],
     };
   } else {
     return state;
