678062e8f7724c6e4cb8b0c80455b4ea88e33326
github username <krishel.lasam@gmail.com>
2017-08-07 01:45:04 -0400

    use action creators to create action objects [chat_intermediate]

--- redux/chat_intermediate/src/App.js
+++ redux/chat_intermediate/src/App.js
@@ -91,6 +91,28 @@ function messagesReducer(state = [], action) {
 
 const store = createStore(reducer);
 
+function deleteMessage(id) {
+  return {
+    type: 'DELETE_MESSAGE',
+    id: id,
+  };
+}
+
+function addMessage(text, threadId) {
+  return {
+    type: 'ADD_MESSAGE',
+    text: text,
+    threadId: threadId,
+  };
+}
+
+function openThread(id) {
+  return {
+    type: 'OPEN_THREAD',
+    id: id,
+  };
+}
+
 const App = () => (
   <div className='ui segment'>
     <ThreadTabs />
@@ -131,10 +153,7 @@ const mapStateToTabsProps = (state) => {
 const mapDispatchToTabsProps = (dispatch) => (
   {
     onClick: (id) => (
-      dispatch({
-        type: 'OPEN_THREAD',
-        id: id,
-      })
+      dispatch(openThread(id))
     ),
   }
 );
@@ -224,10 +243,7 @@ const mapStateToThreadProps = (state) => (
 const mapDispatchToThreadProps = (dispatch) => (
   {
     onMessageClick: (id) => (
-      dispatch({
-        type: 'DELETE_MESSAGE',
-        id: id,
-      })
+      dispatch(deleteMessage(id))
     ),
     dispatch: dispatch,
   }
@@ -238,11 +254,7 @@ const mergeThreadProps = (stateProps, dispatchProps) => (
     ...stateProps,
     ...dispatchProps,
     onMessageSubmit: (text) => (
-      dispatchProps.dispatch({
-        type: 'ADD_MESSAGE',
-        text: text,
-        threadId: stateProps.thread.id,
-      })
+      dispatchProps.dispatch(addMessage(text, stateProps.thread.id))
     ),
   }
 );
