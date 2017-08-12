645454efbbf79ee2ce41cbaa15d2e84c62e29208
github username <krishel.lasam@gmail.com>
2017-08-12 12:50:47 -0400

    define TimersDashboard [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,1 +1,15 @@
 
+class TimersDashboard extends React.Component {
+    render() {
+      return (
+        <div className='ui three column centered grid'>
+          <div className='column'>
+            <EditableTimerList />
+            <ToggleableTimerForm
+              isOpen={true}
+            />
+          </div>
+        </div>
+      );
+    }
+  }
