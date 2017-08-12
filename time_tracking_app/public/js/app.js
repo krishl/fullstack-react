b385c45400b5d0ac2e689ee8b0db51e4c61ee950
github username <krishel.lasam@gmail.com>
2017-08-12 12:54:07 -0400

    define EditableTimerList [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -13,3 +13,26 @@ class TimersDashboard extends React.Component {
       );
     }
   }
+
+  class EditableTimerList extends React.Component {
+    render() {
+      return (
+        <div id='timers'>
+          <EditableTimer
+            title='Learn React'
+            project='Web Domination'
+            elapsed='8986300'
+            runningSince={null}
+            editFormOpen={false}
+          />
+          <EditableTimer
+            title='Learn extreme ironing'
+            project='World Domination'
+            elapsed='3890985'
+            runningSince={null}
+            editFormOpen={true}
+          />
+        </div>
+      );
+    }
+  }
