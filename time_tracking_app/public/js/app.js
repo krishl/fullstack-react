ba29ab33a948db10c46cdf2ed1de82b174b4bf29
github username <krishel.lasam@gmail.com>
2017-08-12 13:06:47 -0400

    define TimerForm [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -58,3 +58,33 @@ class TimersDashboard extends React.Component {
       }
     }
   }
+
+  class TimerForm extends React.Component {
+    render() {
+      const submitText = this.props.title ? 'Update' : 'Create';
+      return (
+        <div className='ui centered card'>
+          <div className='content'>
+            <div className='ui form'>
+              <div className='field'>
+                <label>Title</label>
+                <input type='text' defaultValue={this.props.title} />
+              </div>
+              <div className='field'>
+                <label>Project</label>
+                <input type='text' defaultValue={this.props.project} />
+              </div>
+              <div className='ui two bottom attached buttons'>
+                <button className='ui basic blue button'>
+                  {submitText}
+                </button>
+                <button className='ui basic red button'>
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
