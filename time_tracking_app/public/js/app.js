1bd49507bd71ae96176074345b0ac5c7637b0629
github username <krishel.lasam@gmail.com>
2017-08-12 13:30:24 -0400

    define Timer [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,4 +1,3 @@
-
 class TimersDashboard extends React.Component {
     render() {
       return (
@@ -14,7 +13,7 @@ class TimersDashboard extends React.Component {
     }
   }
 
-  class EditableTimerList extends React.Component {
+class EditableTimerList extends React.Component {
     render() {
       return (
         <div id='timers'>
@@ -89,7 +88,7 @@ class TimersDashboard extends React.Component {
     }
   }
 
-  class ToggleableTimerForm extends React.Component {
+class ToggleableTimerForm extends React.Component {
     render() {
       if (this.props.isOpen) {
         return (
@@ -105,3 +104,38 @@ class TimersDashboard extends React.Component {
         );
       }
     }
+  }
+
+  class Timer extends React.Component {
+    render() {
+      const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+      return (
+        <div className='ui centered card'>
+          <div className='content'>
+            <div className='header'>
+              {this.props.title}
+            </div>
+            <div className='meta'>
+              {this.props.project}
+            </div>
+            <div className='center aligned description'>
+              <h2>
+                {elapsedString}
+              </h2>
+            </div>
+            <div className='extra content'>
+              <span className='right floated edit icon'>
+                <i className='edit icon' />
+              </span>
+              <span className='right floated trash icon'>
+                <i className='trash icon' />
+              </span>
+            </div>
+          </div>
+          <div className='ui bottom attached blue basic button'>
+            Start
+          </div>
+        </div>
+      );
+    }
+  }
