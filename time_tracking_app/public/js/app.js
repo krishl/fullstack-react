93a41b8a2b0c54e3591143c3e9e03ccca3fac74f
github username <krishel.lasam@gmail.com>
2017-08-15 18:57:42 -0400

    add state to ToggleableTimerForm [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -114,15 +114,26 @@ class EditableTimerList extends React.Component {
   }
 
 class ToggleableTimerForm extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  handleFormOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
     render() {
-      if (this.props.isOpen) {
+      if (this.state.isOpen) {
         return (
           <TimerForm />
         );
       } else {
         return (
           <div className='ui basic content center aligned segment'>
-            <button className='ui basic button icon'>
+            <button 
+              className='ui basic button icon'
+              onClick={this.handleFormOpen}
+            >
               <i className='plus icon' />
             </button>
           </div>
