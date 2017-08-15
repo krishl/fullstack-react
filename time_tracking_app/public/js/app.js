f8d967b2134405ccc22ba29a08585380aa48a6da
github username <krishel.lasam@gmail.com>
2017-08-15 13:04:05 -0400

    receive props in EditableTimerList [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -36,22 +36,20 @@ class TimersDashboard extends React.Component {
 
 class EditableTimerList extends React.Component {
     render() {
+      const timers = this.props.timers.map((timer) => (
+        <EditableTimer
+          key={timer.id}
+          id={timer.id}
+          title={timer.title}
+          project={timer.project}
+          elapsed={timer.elapsed}
+          runningSince={timer.runningSince}
+        />
+      ));
+
       return (
         <div id='timers'>
-          <EditableTimer
-            title='Learn React'
-            project='Web Domination'
-            elapsed='8986300'
-            runningSince={null}
-            editFormOpen={false}
-          />
-          <EditableTimer
-            title='Learn extreme ironing'
-            project='World Domination'
-            elapsed='3890985'
-            runningSince={null}
-            editFormOpen={true}
-          />
+          {timers}
         </div>
       );
     }
