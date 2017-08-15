c9b90f29494aa94e4b7373538abd9238da64a3d3
github username <krishel.lasam@gmail.com>
2017-08-15 12:54:25 -0400

    add state to TimersDashboard [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,9 +1,30 @@
 class TimersDashboard extends React.Component {
+  state = {
+    timers: [
+      {
+        title: 'Practice squat',
+        project: 'Gym Chores',
+        id: uuid.v4(),
+        elapsed: 5456099,
+        runningSince: Date.now(),
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuid.v4(),
+        elapsed: 1273998,
+        runningSince: null,
+      },
+    ],
+  };
+
     render() {
       return (
         <div className='ui three column centered grid'>
           <div className='column'>
-            <EditableTimerList />
+            <EditableTimerList
+              timers={this.state.timers}
+            />
             <ToggleableTimerForm
               isOpen={true}
             />
