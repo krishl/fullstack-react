1c793385442f43151f1db08685c161935a5ced8d
github username <krishel.lasam@gmail.com>
2017-08-15 19:22:11 -0400

    add inverse data flow to create timers [time_tracking_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -18,6 +18,17 @@ class TimersDashboard extends React.Component {
     ],
   };
 
+  handleCreateFormSubmit = (timer) => {
+    this.createTimer(timer);
+  };
+
+  createTimer = (timer) => {
+    const t = helpers.newTimer(timer);
+    this.setState({
+      timers: this.state.timers.concat(t),
+    });
+  };
+
     render() {
       return (
         <div className='ui three column centered grid'>
@@ -26,7 +37,7 @@ class TimersDashboard extends React.Component {
               timers={this.state.timers}
             />
             <ToggleableTimerForm
-              isOpen={true}
+              onFormSubmit={this.handleCreateFormSubmit}
             />
           </div>
         </div>
@@ -97,8 +108,16 @@ class EditableTimerList extends React.Component {
       this.setState({ project: e.target.value });
     };
 
+    handleSubmit = () => {
+      this.props.onFormSubmit({
+        id: this.props.id,
+        title: this.state.title,
+        project: this.state.project,
+      });
+    };
+
     render() {
-      const submitText = this.props.title ? 'Update' : 'Create';
+      const submitText = this.props.id ? 'Update' : 'Create';
       return (
         <div className='ui centered card'>
           <div className='content'>
@@ -120,10 +139,16 @@ class EditableTimerList extends React.Component {
                 />
               </div>
               <div className='ui two bottom attached buttons'>
-                <button className='ui basic blue button'>
+                <button
+                  className='ui basic blue button'
+                  onClick={this.handleSubmit}
+                >
                   {submitText}
                 </button>
-                <button className='ui basic red button'>
+                <button
+                  className='ui basic red button'
+                  onClick={this.props.onFormClose}
+                >
                   Cancel
                 </button>
               </div>
@@ -143,10 +168,22 @@ class ToggleableTimerForm extends React.Component {
     this.setState({ isOpen: true });
   };
 
+  handleFormClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  handleFormSubmit = (timer) => {
+    this.props.onFormSubmit(timer);
+    this.setState({ isOpen: false });
+  };
+
     render() {
       if (this.state.isOpen) {
         return (
-          <TimerForm />
+          <TimerForm
+            onFormSubmit={this.handleFormSubmit}
+            onFormClose={this.handleFormClose}
+          />
         );
       } else {
         return (
