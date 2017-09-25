1b5e5e5b9d91a662b976feacb0febc95ee59ec39
github username <krishel.lasam@gmail.com>
2017-09-24 21:43:28 -0400

    add timing functionality [time_tracking_App]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -259,12 +259,20 @@ class ToggleableTimerForm extends React.Component {
   }
 
   class Timer extends React.Component {
+    componentDidMount() {
+      this.forceUpdateInterval = setInterval(() => this.forceUpdate(), 50);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.forceUpdateInterval);
+    }
+
     handleTrashClick = () => {
       this.props.onTrashClick(this.props.id)
     }
 
     render() {
-      const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+      const elapsedString = helpers.renderElapsedString(this.props.elapsed, this.props.runningSince);
       return (
         <div className='ui centered card'>
           <div className='content'>
