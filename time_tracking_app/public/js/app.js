e4ece26c4ed43f492b530bdbdd395363ab285a0c
github username <krishel.lasam@gmail.com>
2017-09-24 22:03:03 -0400

    create TimerActionButton [time_tracker_app]

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -323,6 +323,30 @@ class ToggleableTimerForm extends React.Component {
     }
   }
 
+  class TimerActionButton extends React.Component {
+    render() {
+      if (this.props.timerIsRunning) {
+        return (
+          <div
+            className='ui bottom attached red basic button'
+            onClick={this.props.onStopClick}
+          >
+            Stop
+          </div>
+        );
+      } else {
+        return (
+          <div
+            className='ui bottom attached green basic button'
+            onClick={this.props.onStartClick}
+          >
+            Start
+          </div>
+        );
+      }
+    }
+  }
+
   ReactDOM.render(
     <TimersDashboard />,
     document.getElementById('content')
